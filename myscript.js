b014213d87288d11e7d3f2f1f21639d7a6ba25b4
chenkailei <ckl5975@126.com>
2019-12-17 16:35:43 +0800

    add jquery.js

--- myscript.js
+++ myscript.js
@@ -1,11 +1,35 @@
-function clearAd(){
-	if(document.getElementById("kp_box_57")){
-		document.getElementById("kp_box_57").remove()
-		console.log('广告已清除')
-	}else{
-		setTimeout(()=>{
-			clearAd()
-		},1000)
-	}
+function clearAd() {
+  if ($(".recommend-recommend-box") || $("div[data-pid]") || document.getElementsByClassName("right_extension") || document.getElementsByClassName("banner-ad-box") || $("li[data-type='ad']")) {
+    if ($(".recommend-recommend-box")) {
+      Array.from($(".recommend-recommend-box")).forEach(item => {
+        item.remove()
+      })
+    }
+    if ($("div[data-pid]")) {
+      Array.from($("div[data-pid]")).forEach(item => {
+        item.remove()
+      })
+    }
+    if (document.getElementsByClassName("right_extension")) {
+      Array.from(document.getElementsByClassName("right_extension")).forEach((item) => {
+        item.remove()
+      })
+    }
+    if (document.getElementsByClassName("banner-ad-box")) {
+      if (document.getElementsByClassName("banner-ad-box").length !== 0) {
+        document.getElementsByClassName("banner-ad-box")[0].remove()
+      }
+    }
+    if ($("li[data-type='ad']")) {
+      Array.from($("li[data-type='ad']")).forEach(item => {
+        item.remove()
+      })
+    }
+  } else {
+    setTimeout(() => {
+      clearAd()
+    }, 500);
+  }
+
 }
-clearAd()
+window.onload = clearAd()
