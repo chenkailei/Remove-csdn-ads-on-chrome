ccebfd5783635fc7c63b236b14dacecf0ea84285
chenkailei <ckl5975@126.com>
2019-12-17 16:51:00 +0800

    syl

--- myscript.js
+++ myscript.js
@@ -1,11 +1,11 @@
 function clearAd() {
-  if ($(".recommend-recommend-box") || $(".right_top") || $("div[data-pid]") || document.getElementsByClassName("right_extension") || document.getElementsByClassName("banner-ad-box") || $("li[data-type='ad']")) {
+  if ($(".recommend-recommend-box") || $(".right_top") || $("div[data-pid]") || $(".right_extension") || $(".banner-ad-box") || $("li[data-type='ad']")) {
     if ($(".recommend-recommend-box")) {
       Array.from($(".recommend-recommend-box")).forEach(item => {
         item.remove()
       })
     }
-	if ($(".right_top")) {
+    if ($(".right_top")) {
       Array.from($(".right_top")).forEach(item => {
         item.remove()
       })
@@ -15,14 +15,14 @@ function clearAd() {
         item.remove()
       })
     }
-    if (document.getElementsByClassName("right_extension")) {
-      Array.from(document.getElementsByClassName("right_extension")).forEach((item) => {
+    if ($(".right_extension")) {
+      Array.from($(".right_extension")).forEach((item) => {
         item.remove()
       })
     }
-    if (document.getElementsByClassName("banner-ad-box")) {
-      if (document.getElementsByClassName("banner-ad-box").length !== 0) {
-        document.getElementsByClassName("banner-ad-box")[0].remove()
+    if ($(".banner-ad-box")) {
+      if ($(".banner-ad-box").length !== 0) {
+        $(".banner-ad-box")[0].remove()
       }
     }
     if ($("li[data-type='ad']")) {
